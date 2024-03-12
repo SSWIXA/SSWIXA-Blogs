cddcedef34f7c24e36a308354a1cf44a43666cdc
SSWIXA <76797640+SSWIXA@users.noreply.github.com>
2024-03-12 09:12:49 +0800

    更新布局容器

--- MyBolgs/src/App.vue
+++ MyBolgs/src/App.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './views/Header.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="common-layout">
+    <el-container>
+      <el-header><Header/></el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
